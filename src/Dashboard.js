788f0ffab5208ab42a58a0b6a6a5977d12133215
Blessly Pera <pera.blessly@yahoo.com>
2021-09-20 13:13:03 +0800

    added state and tailwind

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,27 +1,45 @@
 import './css/Dashboard.css';
+import React, {useState, useEffect} from 'react';
 
 function Dashboard() {
 
-    const imgSrc = "https://source.unsplash.com/weekly?beach/1920x1080    ";
-    const date = new Date();
-    const hours = date.getHours();
-    const minutes = date.getMinutes();
-    let timeOfDay = "";
+    const imgSrc = "https://source.unsplash.com/weekly?beach/1920x1080";
+    const [hours, setHours] = useState(null);
+    const [minutes, setMinutes] = useState(null);
+    const [timeOfDay, setTimeOfDay] = useState("");
 
-    if (hours < 12) {
-        timeOfDay = "Morning"
-    } else if (hours >= 12 && hours < 17) {
-        timeOfDay = "Afternoon"
-    } else {
-        timeOfDay = "Evening"
-    }
-  
+    useEffect(() => {
+        const interval = setInterval(() => {
+            const date = new Date();
+            const nextHours = date.getHours();
+            setHours(nextHours);
+            const nextMin = date.getMinutes();
+            setMinutes(nextMin);
+
+            if (nextHours < 12) {
+                setTimeOfDay("Morning");
+            } else if (nextHours >= 12 && nextHours < 17) {
+                setTimeOfDay("Afternoon");
+            } else {
+                setTimeOfDay("Evening");
+            }
+
+            if (nextMin < 10) {
+                setMinutes(nextMin.toString().padStart(2, '0'))
+            }     
+
+        }, 500);
+        return () => {
+          clearInterval(interval);
+        };
+      }, [hours]);
+
    
     return(
         <div className="bg background-tint" style={{backgroundImage: `url(${imgSrc})`}}>
-            <div className="greeting-container">
-            <h1 className="time">{hours}:{minutes}</h1>
-            <h1 className="greeting">Good {timeOfDay}, Blessly!</h1>
+            <div className="h-full flex flex-col items-center justify-center">
+            <h1 className="text-9xl text-white p-10">{hours}:{minutes}</h1>
+            <h1 className="text-2xl text-white 2xl:text-8xl lg:text-7xl md:text-5xl">Good {timeOfDay}, Blessly!</h1>
             </div>
         </div>
     );
